6ebbab92c52c4e0ee5af783e30d7d6a14953213e
yui421 <banhgao.nya@gmail.com>
2020-11-10 11:58:31 +0700

    feat [142]: Change Group Name

---
+++ src/api/messenger/chatThread/changeName.ts
@@ -0,0 +1,29 @@
+import { IGraphqlRequestRequirement, IMqttConnectOptions } from '../../../types'
+import { MqttClient } from 'mqtt'
+import requestGraphQL from '../utils/requestGraphql'
+
+export = (client: MqttClient, options: IMqttConnectOptions) => async function changeName(threadID: number, name: string): Promise<boolean> {
+  
+  const requestData: IGraphqlRequestRequirement = {
+    variables: {
+      "input": {
+        "new_thread_name": name,
+        "source": "SETTINGS",
+        "thread_id": threadID,
+        "actor_id": options.selfFacebookID,
+        "client_mutation_id": "2"
+      }
+    },
+    userAgent: options.userAgent,
+    selfFacebookID: options.selfFacebookID,
+    cookie: options.cookie,
+    fbDtsg: options.fbDtsg,
+    fbApiCallerClass: 'RelayModern',
+    fbApiReqFriendlyName: 'useMessengerThreadNameMutation',
+    docID: '2532865893436941',
+  }
+
+  const rawResp = await requestGraphQL(requestData)
+  
+  return true
+}
